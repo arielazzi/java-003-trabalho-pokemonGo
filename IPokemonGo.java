0e61ffbf6d3218781d9e3c3166f1349c675c7109
Ariel S. Azzi <ariel_azzi@hotmail.com>
2017-04-05 13:14:42 -0300

    Add files via upload
    Inseri a Interface

---
+++ IPokemonGo.java
@@ -0,0 +1,19 @@
+
+
+import java.io.File;
+import java.io.IOException;
+
+public interface IPokemonGo {
+
+	public void loadMatrix(File file) throws IOException;
+		
+	public Chromosome generateChromosome();
+
+	public void insertOrderedChromosome(Chromosome chromosome);
+
+	public int distance(int[] genes);
+
+	public void evolve();
+
+	public void print();
+}
